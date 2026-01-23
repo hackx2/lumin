03927b4ad4d8ab8fa8440de3cdecf73a9b6ad944
HackDev <william.patonlol@gmail.com>
2026-01-23 03:18:01 +0000

    add status

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,14 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType, PresenceUpdateStatus } = require("discord.js");
 
 module.exports = {
     id: Events.ClientReady,
     once: true,
-    async run(_) {
+    async run(client) {
         console.log(`Lumin has awoken... 🌙🌙`);
+
+        client.user.setPresence({
+            activities: [{ name: 'LoFi 🌙', type: ActivityType.Listening }],
+            status: PresenceUpdateStatus.Idle
+        });
     }
 }
